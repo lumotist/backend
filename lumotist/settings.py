57858ebd4fcd69a26f8e0b48ba9ab1d76133c543
xemeds <xemeds@protonmail.com>
2021-01-11 11:43:14 +0300

    Added allowed cors origins

--- lumotist/settings.py
+++ lumotist/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     # Created apps
     'account.apps.AccountConfig',
@@ -64,11 +65,16 @@ AUTH_USER_MODEL = "account.User"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://lumotist.herokuapp.com",
 ]
 
 ROOT_URLCONF = 'lumotist.urls'
