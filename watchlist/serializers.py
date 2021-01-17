2444a37499403432b5e4190817ebb18c7f7ca64c
xemeds <xemeds@protonmail.com>
2021-01-17 22:56:30 +0300

    Updated the watchlist endpoint

--- watchlist/serializers.py
+++ watchlist/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Watchlist
+from account.models import User
 from .models import (
 	WATCHLIST_NAME_MIN_LENGTH,
 	WATCHLIST_NAME_MAX_LENGTH
@@ -8,6 +9,11 @@ from .models import (
 NAME_FIELD = serializers.CharField(min_length=WATCHLIST_NAME_MIN_LENGTH, max_length=WATCHLIST_NAME_MAX_LENGTH, write_only=True, required=True)
 
 class WatchlistSerializer(serializers.ModelSerializer):
+	author = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+     )
+
 	class Meta:
 		model = Watchlist
 		fields = ["id", "name", "public", "animes", "author", "created", "updated", "views"]
--- watchlist/views.py
+++ watchlist/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Watchlist
 from .serializers import (
 	WatchlistSerializer
@@ -10,7 +11,17 @@ from .serializers import (
 @api_view(["GET"])
 def watchlist(request, id):
 	data = {}
-	data["success"] = True
-	data["data"] = WatchlistSerializer(Watchlist.objects.filter(id=id).first()).data
+	try:
+		watchlist = Watchlist.objects.get(id=id)
+		watchlist.views += 1
+		watchlist.save()
+		data["success"] = True
+		data["data"] = WatchlistSerializer(watchlist).data
+	except ObjectDoesNotExist:
+		data["success"] = False
+		data["error"] = "This watchlist does not exist or maybe it was deleted."
 
-	return Response(data)
+	if data["success"]:
+		return Response(data)
+	else:
+		return Response(data, status=status.HTTP_400_BAD_REQUEST)
