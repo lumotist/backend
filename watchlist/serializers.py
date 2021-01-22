eeeb366f882e418116e2267d55c336176b843b8f
xemeds <xemeds@protonmail.com>
2021-01-22 14:27:53 +0300

    Added the unique name restriction to the update name endpoint

--- watchlist/serializers.py
+++ watchlist/serializers.py
@@ -59,12 +59,12 @@ class CreateSerializer(serializers.Serializer):
 
 class UpdateNameSerializer(serializers.Serializer):
 	id = ID_FIELD
-	name = NAME_FIELD
+	new_name = NAME_FIELD
 
 	def validate(self, data):
 		user = self.context['request'].user
 		id = data["id"]
-		name = data["name"]
+		new_name = data["new_name"]
 
 		try:
 			self.watchlist = Watchlist.objects.get(id=id)
@@ -74,13 +74,19 @@ class UpdateNameSerializer(serializers.Serializer):
 		if self.watchlist.author != user:
 			raise serializers.ValidationError({'user': ("The auth user is not the author of the watchlist.")})
 
-		if self.watchlist.name == name:
-			raise serializers.ValidationError({'name': ("The new name cannot be the same as the current name.")})
+		if self.watchlist.name == new_name:
+			raise serializers.ValidationError({'new_name': ("The new name cannot be the same as the current name.")})
+
+		try:
+			user.watchlists.get(name=new_name)
+			raise serializers.ValidationError({'new_name': ("You already have a watchlist with that name.")})
+		except ObjectDoesNotExist:
+			pass
 
 		return data
 
 	def save(self):
-		self.watchlist.name = self.validated_data['name']
+		self.watchlist.name = self.validated_data['new_name']
 		self.watchlist.save()
 
 		return self.watchlist
